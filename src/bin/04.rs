a16b58acc998c567ea10bfca197714673284085b
lepelog <25211966+lepelog@users.noreply.github.com>
2022-12-05 19:31:13 +0100

    04p2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -14,12 +14,22 @@ fn includes(r1: &RangeInclusive<usize>, r2: &RangeInclusive<usize>) -> bool {
     r1.start() <= r2.start() && r1.end() >= r2.end()
 }
 
+fn overlap(r1: &RangeInclusive<usize>, r2: &RangeInclusive<usize>) -> bool {
+    !(r1.start() > r2.end() || r1.end() < r2.start())
+}
+
 fn main() {
     let input = include_str!("../input/04.txt");
     let totally_included = input
         .lines()
         .map(parse_ranges)
-        .filter(|(r1, r2)| includes(&r1, &r2) || includes(&r2, &r1))
+        .filter(|(r1, r2)| includes(r1, r2) || includes(r2, r1))
         .count();
     println!("{totally_included}");
+    let total_overlap = input
+        .lines()
+        .map(parse_ranges)
+        .filter(|(r1, r2)| overlap(r1, r2))
+        .count();
+    println!("{total_overlap}");
 }
