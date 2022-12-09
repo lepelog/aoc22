2ce5cbe24e4e7e7ac7416b18d7d375afcd8bf5c6
lepelog <25211966+lepelog@users.noreply.github.com>
2022-12-09 12:00:31 +0100

    08p2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -35,8 +35,10 @@ fn main() {
     println!("{}:{}", grid.columns, grid.rows);
     let directions = [(0,1), (0,-1), (1,0), (-1,0)];
     let mut visible_trees = 0;
+    let mut max_scenic_score = 0;
     for x in 0..grid.columns as isize {
         for y in 0..grid.rows as isize {
+            // part1
             if directions.iter().any(|dir| {
                 // checks if the tree is visible in this direction
                 let mut pos = (x,y);
@@ -54,7 +56,27 @@ fn main() {
             }) {
                 visible_trees += 1;
             }
+            // part2
+            let cur_view_score: usize = directions.iter().map(|dir| {
+                // calculate visible direction
+                let mut view_dist = 0;
+                let mut pos = (x,y);
+                let start_height = grid.get(pos.0, pos.1).unwrap();
+                pos.0 += dir.0;
+                pos.1 += dir.1;
+                while let Some(height) = grid.get(pos.0, pos.1) {
+                    view_dist += 1;
+                    if height >= start_height {
+                        break;
+                    }
+                    pos.0 += dir.0;
+                    pos.1 += dir.1;
+                }
+                view_dist
+            }).product();
+            max_scenic_score = max_scenic_score.max(cur_view_score);
         }
     }
     println!("{}", visible_trees);
+    println!("{}", max_scenic_score);
 }
